11bc588825f413172845ef424e67e38c10c31871
viet-data <vis249x9@gmail.com>
2025-03-05 01:24:48 +0700

    pp

--- src/in_context_reranker.py
+++ src/in_context_reranker.py
@@ -498,8 +498,10 @@ class InContextReranker():
             
             if kv_cache is not None:
                 context_start_idx=query_start_idx
+                context_start_idx_demo=query_start_idx_demo
             else:
                 context_start_idx=0
+                context_start_idx_demo=0
 
             # FP with the actual query            
             llm_prompt, doc_tok_idx_spans, query_start_idx, query_end_idx = self._prepare_input_for_document_retrieval(query, retrieval_doc_pool, system_prompt=prompt_prefix, query_position='last')
@@ -509,7 +511,7 @@ class InContextReranker():
         
             llm_prompt_demo, doc_tok_idx_spans_demo, query_start_idx_demo, query_end_idx_demo = self._prepare_input_for_demo_retrieval(query, retrieval_doc_pool, system_prompt=prompt_prefix, query_position='last')
         
-            doc_scores_query_demo, perdoc_result_demo = self.score_documents_demo(retrieval_doc_pool, llm_prompt_demo, doc_tok_idx_spans_demo, query_start_idx_demo, query_end_idx_demo, return_per_doc_results=return_per_doc_results,  kv_cache=kv_cache, context_start_idx=context_start_idx)
+            doc_scores_query_demo, perdoc_result_demo = self.score_documents_demo(retrieval_doc_pool, llm_prompt_demo, doc_tok_idx_spans_demo, query_start_idx_demo, query_end_idx_demo, return_per_doc_results=return_per_doc_results,  kv_cache=kv_cache, context_start_idx=context_start_idx_demo)
 
             _i = 0
             doc_scores = torch.zeros(len(retrieval_doc_pool))
