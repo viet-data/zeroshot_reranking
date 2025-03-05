b59e5f9572ec7ecc6c6a76c38e29c23b6d50d85a
viet-data <vis249x9@gmail.com>
2025-03-05 21:05:52 +0700

    ppp

--- src/in_context_reranker.py
+++ src/in_context_reranker.py
@@ -220,7 +220,7 @@ class InContextReranker():
         self.tokenizer.pad_token = self.tokenizer.eos_token
         from src.get_embedding import merge_vectors_slerp, karcher_mean_sphere
         original_length = len(self.tokenizer(doc).input_ids)
-        input_ids = self.tokenizer(doc + "\nSummary content", return_tensors="pt").input_ids.to(self.llm.device)
+        input_ids = self.tokenizer(doc + "\nRead and then summary content", return_tensors="pt").input_ids.to(self.llm.device)
         with torch.no_grad():
             generation_output = self.llm(input_ids=input_ids, 
                                         output_hidden_states=True
