408ab7265ac6f93b8035f83c5b401b68cdc4c182
viet-data <vis249x9@gmail.com>
2025-03-06 12:05:42 +0700

    ppp

--- src/in_context_reranker.py
+++ src/in_context_reranker.py
@@ -230,8 +230,8 @@ class InContextReranker():
                                         )
             old_values = generation_output.hidden_states  
             #old_values = torch.cat([karcher_mean_sphere(i[0]) for i in old_values], dim=0).unsqueeze(1)
-            old_values = torch.cat([(self.gather(i, original_length)) for i in old_values], dim=0)
-            #old_values = torch.cat([(i.mean(dim=1, keepdim=True) + self.gather(i, original_length))/2 for i in old_values], dim=0)
+            #old_values = torch.cat([(self.gather(i, original_length)) for i in old_values], dim=0)
+            old_values = torch.cat([(i.mean(dim=1, keepdim=True) + self.gather(i, original_length))/2 for i in old_values], dim=0)
             dct[doc] = old_values
             return old_values
 
